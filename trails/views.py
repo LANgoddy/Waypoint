c48518dab6de28d2b84c31f10b96ee89b3241ddf
Lucia Ngoddy <langoddy1@gmail.com>
2026-08-18 10:05:29 -0400

    week 13: add tests performance optimization and logging

--- trails/views.py
+++ trails/views.py
@@ -1,3 +1,6 @@
+# Import Python's built-in logging module.
+import logging
+
 # Import render so Django can display HTML templates.
 from django.shortcuts import render
 
@@ -12,11 +15,22 @@ from .forms import TrailSearchForm
 
 
 # ---------------------------------------------------------
+# LOGGER
+# ---------------------------------------------------------
+
+# Create a logger for this trails application.
+logger = logging.getLogger(__name__)
+
+
+# ---------------------------------------------------------
 # ABOUT VIEW
 # ---------------------------------------------------------
 
 # This view handles requests for the About page.
 def about(request):
+
+    # Record that the About page was requested.
+    logger.info("About page requested.")
 
     # Render the About page HTML template.
     return render(request, "trails/about.html")
@@ -41,20 +55,31 @@ class TrailListView(ListView):
     # Show only two trails on each page.
     paginate_by = 2
 
-    # Filter the Trail records based on the user's search.
+    # Get and filter the Trail records displayed on the page.
     def get_queryset(self):
 
-        # Start with all Trail records.
-        queryset = Trail.objects.all()
+        # Record that the trail catalogue was requested.
+        logger.info("Trail catalogue requested.")
+
+        # Retrieve Trails and their related Parks efficiently.
+        queryset = Trail.objects.select_related("park").all()
 
         # Get the text entered in the search box.
         query = self.request.GET.get("query")
 
         # If the user entered a search term, filter by trail name.
         if query:
+
+            # Record the search term in the log.
+            logger.info("Trail search performed for: %s", query)
+
+            # Filter Trail records by name.
             queryset = queryset.filter(name__icontains=query)
 
-        # Return the final list of Trail records.
+        # Give the queryset a consistent order for pagination.
+        queryset = queryset.order_by("name")
+
+        # Return the final Trail records.
         return queryset
 
     # Add extra information that the template can use.
@@ -85,3 +110,12 @@ class TrailDetailView(DetailView):
 
     # Give the selected Trail object a clear name in the template.
     context_object_name = "trail"
+
+    # Retrieve the Trail and its related Park efficiently.
+    def get_queryset(self):
+
+        # Record that a trail detail page was requested.
+        logger.info("Trail detail page requested.")
+
+        # Retrieve the Trail and Park in the same database query.
+        return Trail.objects.select_related("park")
